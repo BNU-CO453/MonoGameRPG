513eabc9024f18fa98855d3421f49348355dbd32
Derek Peacock <derek.peacock@yahoo.com>
2021-03-02 20:53:01 +0000

    Add project files.

---
+++ MonoGameRPG/RPG_Game.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameRPG
+{
+    public class RPG_Game : Game
+    {
+        // constants
+
+        public const int HD_Height = 720;
+        public const int HD_Width = 1280;
+
+        // Attributes/variables
+
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private Texture2D playerImage;
+
+        private Texture2D walkDownImages;
+        private Texture2D walkUpImages;
+        private Texture2D walkLeftImages;
+        private Texture2D walkRightImages;
+
+        private Texture2D backgroundImage;
+        private Texture2D ballImage;
+        private Texture2D skullImage;
+
+        public RPG_Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Setup the screen to the preferred size in
+        /// this example it is HD 720p (1280 x 720)
+        /// full HD is 1080P (1920 x 1080) pixells
+        /// </summary>
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = HD_Width;
+            graphics.PreferredBackBufferHeight = HD_Height;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Load all the images for the game from the
+        /// Content pipeline.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            playerImage = Content.Load<Texture2D>("Player/player");
+            
+            walkDownImages = Content.Load<Texture2D>("Player/walkDown");
+            walkUpImages = Content.Load<Texture2D>("Player/walkUp");
+            walkRightImages = Content.Load<Texture2D>("Player/walkRight");
+            walkLeftImages = Content.Load<Texture2D>("Player/walkLeft");
+
+            backgroundImage = Content.Load<Texture2D>("background");
+            ballImage = Content.Load<Texture2D>("ball");
+            skullImage = Content.Load<Texture2D>("skull");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            Vector2 position = new Vector2(0, 0);
+
+            spriteBatch.Draw(
+                backgroundImage, position, Color.White);
+
+            spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
